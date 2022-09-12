bba766a886c188c34af9141c6f896d584b0977bc
Wasjusja <91008731+Wasjusja@users.noreply.github.com>
2022-09-12 22:33:39 +0300

    Update variables.tf
    3

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "YANDEX_TOKEN" {}
 
 variable "yandex_folder_id" {
-  default = "b1g5jatgvp7evt4p5s99"
+  default = "b1glcs1c44jip4ce4jcb"
 }
 
 variable "yandex_cloud_id" {
-  default = "b1grvrc5vladcufpdjjn"
+  default = "b1g5jatgvp7evt4p5s99"
 }
 
 variable "SSH_ID_RSA_PUB" {}
