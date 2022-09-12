ccae631e7404857482c9f4229dbcb329da2c4d14
Wasjusja <91008731+Wasjusja@users.noreply.github.com>
2022-09-12 09:15:18 +0300

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,79 @@
+output "entrance_zagovorov_ru_ip_addr_external" {
+  value = yandex_compute_instance.entrance_instance.network_interface.0.nat_ip_address
+}
+
+output "db01_zagovorov_ru_ip_addr_internal" {
+  value = yandex_compute_instance.db01_instance.network_interface.0.ip_address
+}
+
+output "db02_zagovorov_ru_ip_addr_internal" {
+  value = yandex_compute_instance.db02_instance.network_interface.0.ip_address
+}
+
+output "app_zagovorov_ru_ip_addr_internal" {
+  value = yandex_compute_instance.app_instance.network_interface.0.ip_address
+}
+
+output "monitoring_zagovorov_ru_ip_addr_internal" {
+  value = yandex_compute_instance.monitoring_instance.network_interface.0.ip_address
+}
+
+output "github_zagovorov_ru_ip_addr_internal" {
+  value = yandex_compute_instance.github_instance.network_interface.0.ip_address
+}
+
+output "runner_zagovorov_ru_ip_addr_internal" {
+  value = yandex_compute_instance.runner_instance.network_interface.0.ip_address
+}
+
+output "ssh_config" {
+  value = <<-EOT
+  Host zagovoroff.ru
+    HostName ${yandex_compute_instance.entrance_instance.network_interface.0.nat_ip_address}
+    User ubuntu
+    IdentityFile ~/.ssh/id_rsa
+
+  Host db01.zagovorov.ru
+    HostName ${yandex_compute_instance.db01_instance.network_interface.0.ip_address}
+    User ubuntu
+    IdentityFile ~/.ssh/id_rsa
+      ProxyJump ubuntu@${yandex_compute_instance.nat_instance.network_interface.0.nat_ip_address}
+      ProxyCommand ssh -W %h:%p -i .ssh/id_rsa
+
+  Host db02.zagovorov.ru
+    HostName ${yandex_compute_instance.db02_instance.network_interface.0.ip_address}
+    User ubuntu
+    IdentityFile ~/.ssh/id_rsa
+      ProxyJump ubuntu@${yandex_compute_instance.nat_instance.network_interface.0.nat_ip_address}
+      ProxyCommand ssh -W %h:%p -i .ssh/id_rsa
+
+  Host app.zagovorov.ru
+    HostName ${yandex_compute_instance.app_instance.network_interface.0.ip_address}
+    User ubuntu
+    IdentityFile ~/.ssh/id_rsa
+      ProxyJump ubuntu@${yandex_compute_instance.nat_instance.network_interface.0.nat_ip_address}
+      ProxyCommand ssh -W %h:%p -i .ssh/id_rsa
+
+  Host monitoring.zagovorov.ru
+    HostName ${yandex_compute_instance.monitoring_instance.network_interface.0.ip_address}
+    User ubuntu
+    IdentityFile ~/.ssh/id_rsa
+      ProxyJump ubuntu@${yandex_compute_instance.nat_instance.network_interface.0.nat_ip_address}
+      ProxyCommand ssh -W %h:%p -i .ssh/id_rsa
+
+  Host gitlab.zagovorov.ru
+    HostName ${yandex_compute_instance.gitlab_instance.network_interface.0.ip_address}
+    User ubuntu
+    IdentityFile ~/.ssh/id_rsa
+      ProxyJump ubuntu@${yandex_compute_instance.nat_instance.network_interface.0.nat_ip_address}
+      ProxyCommand ssh -W %h:%p -i .ssh/id_rsa
+
+  Host runner.zagovorov.ru
+    HostName ${yandex_compute_instance.runner_instance.network_interface.0.ip_address}
+    User ubuntu
+    IdentityFile ~/.ssh/id_rsa
+      ProxyJump ubuntu@${yandex_compute_instance.nat_instance.network_interface.0.nat_ip_address}
+      ProxyCommand ssh -W %h:%p -i .ssh/id_rsa
+
+  EOT
+}
